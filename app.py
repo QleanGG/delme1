921542b767ea2fd2eafae525e45140587b9c849d
QleanGG <guyguz1@gmail.com>
2024-03-05 12:06:48 +0200

    add data

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import json
 app = Flask(__name__)
+
+data = [1,2,3,45]
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/get_data')
+def get_data():
+    return json.dumps(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
